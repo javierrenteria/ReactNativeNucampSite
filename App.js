3ccc9306f18fdc3fd809db1a5f43ea62e5b5901b
Javier Renteria <javierrenteriamendez@gmail.com>
2021-05-15 01:29:03 -0700

    RN Components Part 1

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Main />
   );
 }
 
